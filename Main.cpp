e01162e1b049d1d83afbff9012b9a72fffc4cba0
Roy <royskim77@gmail.com>
2020-01-06 16:20:32 +0900

    Initial Commit

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+		return 1;
+
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello People of the World!" << std::endl;
+	std::cout << "This day forward, you get to know who we fight against in this battle" << std::endl;
+	std::cout << "However, some of us would feel uncertain why the fight has to come" << std::endl;
+	std::cout << "I give you the reason." << std::endl;
+	std::cout << "Without winning this battle against our enemy up there, you and your family will suffer for generations to come" << std::endl;
+	std::cout << "Hello People of the World!" << std::endl;
+	std::cout << "Hello People of the World!" << std::endl;
+	std::cout << "Hello People of the World!" << std::endl;
+	std::cout << "Hello People of the World!" << std::endl;
+	return 0;
+}
